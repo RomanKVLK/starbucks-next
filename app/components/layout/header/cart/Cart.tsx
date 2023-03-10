ce430cfb1a1c572e89dd12a1f0575a8cd346b5ed
RomanKVLK <romnik20021994@gmail.com>
2023-03-10 16:37:20 +0300

    Cart

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,8 +1,59 @@
 import styles from './Cart.module.scss'
-import { FC } from 'react'
+import CartItem from './cart-item/CartItem'
+import { cart } from '@/data/cart.data'
+import {
+	Button,
+	Drawer,
+	DrawerBody,
+	DrawerCloseButton,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay
+} from '@chakra-ui/react'
+import { FC, useRef, useState } from 'react'
 
 const Cart: FC = () => {
-	return <div className={styles.cart}>Cart</div>
+	const [isOpen, setIsOpen] = useState(false)
+	const btnRef = useRef<HTMLButtonElement>(null)
+	return (
+		<div className={styles['wrapper-cart']}>
+			<button
+				onClick={() => {
+					setIsOpen(!isOpen)
+				}}
+				className={styles.heading}
+				ref={btnRef}
+			>
+				<div className={styles.badge}>1</div>
+				<span className={styles.text}>My backet</span>
+			</button>
+			<Drawer
+				isOpen={isOpen}
+				placement='right'
+				onClose={() => setIsOpen(false)}
+				finalFocusRef={btnRef}
+			>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+					<DrawerHeader>My cart</DrawerHeader>
+
+					<DrawerBody>
+						<div className={styles.cart}>
+							{cart.map(item => (
+								<CartItem item={item} key={item.id} />
+							))}
+						</div>
+					</DrawerBody>
+
+					<DrawerFooter>
+						<Button colorScheme='green'>Checkout</Button>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
+		</div>
+	)
 }
 
 export default Cart
---
+++ app/data/product.data.ts
@@ -0,0 +1,67 @@
+import { IProduct } from '@/types/product.interface'
+
+export const products: IProduct[] = [
+	{
+		id: 1,
+		description:
+			'Sweet pistachio flavor blended with coffee, milk and ice, then finished with whipped cream and a salted brown-buttery topping—an icy-smooth, creamy delight to bring you into the new year.',
+		name: 'Pistachio Frappuccino® Blended Beverage',
+		images: ['/images/products/Pistachio-Frappuccino.png'],
+		price: 9,
+		reviews: []
+	},
+	{
+		id: 2,
+		description:
+			'Frappuccino® Roast coffee, mocha sauce and Frappuccino® chips blended with milk and ice, layered on top of whipped cream and chocolate cookie crumble and topped with vanilla whipped cream, mocha drizzle and even more chocolate cookie crumble. Each sip is as good as the last . . . all the way to the end.',
+		name: 'Mocha Cookie Crumble Frappuccino',
+		images: ['/images/products/Mocha-Cookie-Crumble-Frappuccino.png'],
+		price: 8,
+		reviews: []
+	},
+	{
+		id: 3,
+		description:
+			'Buttery caramel syrup blended with coffee, milk and ice, then topped with a layer of dark caramel sauce, whipped cream, caramel drizzle and a crunchy caramel-sugar topping—oh-so-beautifully delicious.',
+		name: 'Caramel Ribbon Crunch Frappuccino',
+		images: ['/images/products/Caramel-Ribbon-Crunch-Frappuccino.png'],
+		price: 9,
+		reviews: []
+	},
+	{
+		id: 4,
+		description:
+			'Coffee is combined with a shot of espresso and milk, then blended with ice to give you a nice little jolt and lots of sipping joy.',
+		name: 'Espresso Frappuccino Blended Beverage',
+		images: ['/images/products/Espresso-Frappuccino-Blended-Beverage.png'],
+		price: 11,
+		reviews: []
+	},
+	{
+		id: 5,
+		description:
+			'We blend mocha sauce and Frappuccino® chips with coffee, milk and ice, then top it off with whipped cream and a mocha drizzle to bring you endless java joy.',
+		name: 'Java Chip Frappuccino Blended Beverage',
+		images: ['//images/products/Java-Chip-Frappuccino-Blended-Beverage.png'],
+		price: 10,
+		reviews: []
+	},
+	{
+		id: 6,
+		description:
+			'Mocha sauce, Frappuccino® Roast coffee, milk and ice all come together for a mocha flavor thatll leave you wanting more. To change things up, try it affogato-style with a hot espresso shot poured right over the top.',
+		name: 'Java Chip Frappuccino Blended Beverage',
+		images: ['/images/products/Mocha-Frappuccino-Blended-Beverage.png'],
+		price: 9,
+		reviews: []
+	},
+	{
+		id: 7,
+		description:
+			'We take Frappuccino® roast coffee and vanilla bean powder, combine them with milk and ice, topped with whipped cream. Tastes like happiness.',
+		name: 'Caffè Vanilla Frappuccino® Blended Beverage',
+		images: ['/images/products/Mocha-Frappuccino-Blended-Beverage.png'],
+		price: 11,
+		reviews: []
+	}
+]
